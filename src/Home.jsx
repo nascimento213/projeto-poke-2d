b6254d7703a2499af3ac68e0ba4db6c2dba6f20f
programadormovel <programadormovel@gmail.com>
2025-05-07 08:36:36 -0300

    pokemon

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import poke from './assets/pokeapi_256.png'
+
+const Home = () => {
+
+    return (
+        <div>
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+        </div>
+    )
+}
+
+export default Home
